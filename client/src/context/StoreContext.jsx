d35e1dcc3a5e1fe5272a4743bdf8fff26ea435e9
Francis <kikobilas123@gmail.com>
2024-08-11 00:27:51 +0800

    added a global state variable

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,11 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../menu-list";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] + 1,
+      }));
+    }
+  };
+
+  const removeFromCarts = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1,
+    }));
+  };
+
   const contextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCarts,
   };
 
   return (
