6ac8cd0becaabaa389768458edfa4a12e1ab64f4
Francis <kikobilas123@gmail.com>
2024-08-13 23:50:16 +0800

    Added a Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/cart/Cart";
 import OrderPlace from "./pages/place-order/PlaceOrder";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <main>
-      <BrowserRouter>
+    <BrowserRouter>
+      <main>
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<OrderPlace />} />
         </Routes>
-      </BrowserRouter>
-    </main>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
