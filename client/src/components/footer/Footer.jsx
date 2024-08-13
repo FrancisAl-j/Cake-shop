4e51220d900702a0e5c5beec48870d5ca2e923b5
Francis <kikobilas123@gmail.com>
2024-08-14 00:56:44 +0800

    add some things on the footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,24 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <h1>Cake Shop</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+            inventore magnam tempore quibusdam assumenda soluta eum optio vitae
+            perferendis voluptatibus?
+          </p>
+          <div className="footer-social-icons">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center"></div>
+        <div className="footer-content-right"></div>
+      </div>
     </footer>
   );
 };
