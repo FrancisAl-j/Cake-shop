8d3affeb9e33b2f5cacafae5e23ffc34e66c79a3
Francis <kikobilas123@gmail.com>
2024-08-30 23:55:44 +0800

    create an api endpoint for creating a product

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,7 +2,19 @@ import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 const addProduct = async (req, res) => {
+  const { name, price, description, category, image } = req.body;
+  const image_filename = `${req.file.filename}`;
   try {
+    const food = new foodModel({
+      name,
+      description,
+      price,
+      category,
+      image: image_filename,
+    });
+
+    await food.save();
+    res.json(200).json(food);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
