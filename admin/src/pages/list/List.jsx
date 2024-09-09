cc2fee096fb6081f54de43d28b54c5684917fdba
Francis <kikobilas123@gmail.com>
2024-09-10 00:00:43 +0800

    Added a delete functionality => deleting cakes

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 import "./list.css";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
   const [foods, setFoods] = useState([]);
   useEffect(() => {
-    const fetchFood = async () => {
-      try {
-        const res = await axios.get("http://localhost:3000/api/food/list");
-        setFoods(res.data);
-        console.log(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     fetchFood();
   }, []);
+
+  const fetchFood = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/food/list");
+      setFoods(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemove = async (id, item) => {
+    try {
+      if (window.confirm(`Are you sure you want to remove ${item}`)) {
+        const res = await axios.delete(
+          `http://localhost:3000/api/food/delete/${id}`
+        );
+        await fetchFood();
+        if (res.status === 200) {
+          toast.success("Product successfuly deleted");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="list add flex-col">
       <h1>List</h1>
@@ -36,7 +53,12 @@ const List = () => {
             <p>{food.name}</p>
             <p>{food.category}</p>
             <p>₱{food.price}</p>
-            <p>X</p>
+            <p
+              className="cursor"
+              onClick={() => handleRemove(food._id, food.name)}
+            >
+              X
+            </p>
           </div>
         );
       })}
