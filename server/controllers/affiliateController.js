7cce8808d913f04c4959da07c4550d9fb8dd1a80
Francis <kikobilas123@gmail.com>
2024-10-26 23:49:13 +0800

    Created a signup/registration for account on users. (admin and employee) = Backend

--- server/controllers/affiliateController.js
+++ server/controllers/affiliateController.js
@@ -1,1 +1,44 @@
 import Admin from "../models/adminModel.js";
+import Employee from "../models/employeeModel.js";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const affiliateSignUp = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const existingEmail = await Admin.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already existing." });
+    }
+
+    // Validator
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email address." });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be more than 8 characters." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newAffiliate = new Admin({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    const affiliate = await newAffiliate.save();
+
+    res.status(200).json(affiliate);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export default { affiliateSignUp };
--- server/routes/affiliateRoute.js
+++ server/routes/affiliateRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
+import controller from "../controllers/affiliateController.js";
 
 const router = express.Router();
+
+router.post("/signup", controller.affiliateSignUp); // Sign up/Registration of account
 
 export default router;
