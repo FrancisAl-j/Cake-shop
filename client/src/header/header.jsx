934870e086e40cd856ec11afd2edfd95612ada73
Francis <kikobilas123@gmail.com>
2025-01-25 21:14:25 +0800

    functional button

--- client/src/header/header.jsx
+++ client/src/header/header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favourite cake here</h2>
         <p>Choose a diverse menu featuring different types of cakes</p>
-        <button>View Menu</button>
+        <a href="#explore-menu">
+          <button>View Menu</button>
+        </a>
       </div>
     </div>
   );
