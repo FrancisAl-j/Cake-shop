aadbb4d24a3c9a27af14a13eb774b92fb4f48b56
Francis <kikobilas123@gmail.com>
2025-01-27 23:57:21 +0800

    fix the UI and the code on backend

--- server/controllers/topCakeController.js
+++ server/controllers/topCakeController.js
@@ -3,7 +3,7 @@ import foodModel from "../models/foodModel.js";
 // Shows the top 5 top foods
 const topProducts = async (req, res) => {
   try {
-    const foods = await foodModel.find().sort({ buys: -1 });
+    const foods = await foodModel.find({ buys: { $gt: 0 } }).sort({ buys: -1 }); // Fetch only products with buys > 0
     const results = foods.slice(0, 5);
     res.status(200).json(results);
   } catch (error) {
